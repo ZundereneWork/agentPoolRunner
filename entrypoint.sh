bb8b39b5f6a0086f139db9423875099260a6874d
hector21450 <hector21450@gmail.com>
2023-02-25 13:35:08 +0100

    change entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,5 @@ set -eEuo pipefail
 ACTIONS_RUNNER_INPUT_NAME=$HOSTNAME
 export RUNNER_ALLOW_RUNASROOT=1
 
-TOKEN="$(curl -sS --request POST --url "https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/actions/runners/registration-token" --header "authorization: Bearer ${GH_TOKEN}"  --header 'content-type: application/json' | jq -r .token)"
-
-/actions-runner/config.sh --unattended --replace --work "/tmp" --url "$ACTIONS_RUNNER_INPUT_URL" --token "$TOKEN" --labels aks-runner
-/actions-runner/bin/runsvc.sh
+/actions-runner/config.sh --url "https://github.com/$REPO_OWNER" --token "$GH_TOKEN" --labels "$NAME"
+/actions-runner/run.sh
