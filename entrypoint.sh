4daf4961efb513169c33dfe1cef5b2a2e52a24f0
hector21450 <hector21450@gmail.com>
2023-02-25 14:49:02 +0100

    script

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ ACTIONS_RUNNER_INPUT_NAME=$HOSTNAME
 export RUNNER_ALLOW_RUNASROOT=1
 
 echo $GH_TOKEN
+echo "HOLA ESTA ES MI ORGANIZACION -> " $REPO_OWNER 
 
-
-/actions-runner/config.sh --url "https://github.com/$REPO_OWNER" --token "$GH_TOKEN" --labels "$NAME"
-/actions-runner/run.sh
+#/actions-runner/config.sh --url "https://github.com/$REPO_OWNER" --token $GH_TOKEN --labels $NAME
+#/actions-runner/run.sh
