a0600476147a9f3576e41c25516c2f9f26d9c549
hector.guerreiro <hector21450@gmail.com>
2023-02-26 19:39:28 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,5 @@ set -eEuo pipefail
 ACTIONS_RUNNER_INPUT_NAME=$HOSTNAME
 export RUNNER_ALLOW_RUNASROOT=1
 
-
-
 /actions-runner/config.sh --url "https://github.com/$REPO_OWNER" --token "$GH_TOKEN" --labels "$NAME"
 /actions-runner/run.sh
